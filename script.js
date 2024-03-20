ef5b110033b3cb651e28e68ab81509e6eec751ea
brenda amaya <amayabrenda2017@gmail.com>
2024-03-19 22:24:27 -0300

    entrega final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // dolar a peso
-function dolaresAPesos(dolares, tasaCambio) {
+/*function dolaresAPesos(dolares, tasaCambio) {
     return dolares * tasaCambio;
   }
 const dolares = parseFloat(prompt("Ingrese la cantidad de dolares:"));
@@ -53,15 +53,56 @@ console.log("Por favor, ingrese valores numericos validos.");
 } else {
 const pesosConvertidos = realesAPesos(reales, tasaCambiorealesAPesos);
 console.log(`${reales} reales son ${pesosConvertidos} pesos argentinos.`);
+} */
+
+//login
+function login() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var userData = {
+      nombre: "Usuario",
+      email: email
+  };
+  localStorage.setItem("userEmail", email);
+
+  mostrarDatosUsuario(userData);
+  obtenerDatosDivisas();
 }
 
+function mostrarDatosUsuario(userData) {
+  var elementoDatosUsuario = document.getElementById("userData");
+  elementoDatosUsuario.innerHTML = "<h2>Bienvenido, " + userData.nombre + "</h2><p>Correo electrónico: " + userData.email + "</p>";
+  elementoDatosUsuario.style.display = "block";
+  document.getElementById("loginForm").style.display = "none";
+}
+
+function obtenerDatosDivisas() {
+  var datosDivisas = {
+      ARS: 1,
+      USD: 850.75,
+      EUR: 926.28,
+      BRL: 170.32
+  };
+  var userEmail = localStorage.getItem("userEmail");
+  if (userEmail) {
+      console.log("Correo electrónico del usuario recuperado del almacenamiento local:", userEmail);
+  } else {
+      console.log("Correo electrónico del usuario no encontrado");
+  }
+
+  var elementoDatosDivisas = document.getElementById("currencyData");
+  elementoDatosDivisas.innerHTML = "<h2>Información de divisas</h2>";
+  for (var divisa in datosDivisas) {
+      elementoDatosDivisas.innerHTML += "<p>" + divisa + ": " + datosDivisas[divisa] + "</p>";
+  }
+}
 
 // cambio click
 
 const monedaEl_one = document.getElementById('moneda-uno');
 const monedaEl_two = document.getElementById('moneda-dos');
 const cantidadEl_one = document.getElementById('cantidad-uno');
-const cantidadEl_two = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
 const cambioEl = document.getElementById('cambio');
 const tazaEl = document.getElementById('taza');
 
@@ -70,12 +111,12 @@ function calculate(){
   const moneda_one = monedaEl_one.value;
   const moneda_two = monedaEl_two.value;
 
- fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
- .then(res => res.json() )
- .then(data => {
-     const taza = data.rates[moneda_two];
+fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+.then(res => res.json() )
+.then(data => {
+    const taza = data.rates[moneda_two];
 
-     cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+    cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
 
      cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
 
@@ -91,6 +132,17 @@ cantidadEl_two.addEventListener('input', calculate);
 
 calculate();
 
+// API- ploty
+var data = [
+  {
+    x: ['2024-01-01 11:00:00', '2024-02-01 11:00:00', '2024-03-01 11:00:00'],
+    y: [2, 3, 6],
+    type: 'scatter'
+  }
+  
+];
+
+Plotly.newPlot('myDiv', data);
 
 
 
@@ -101,28 +153,28 @@ const diferentesDivisas = [
     moneda: "peso argentino",
     simbolo: "$",
     ambito: "argentina",
-    tasaMomentanea: "950",
+    tasaMomentanea: "1",
     codigoIso:	"ARS",
   },
   {
     moneda: "dolar",
     simbolo: "$",
     ambito: "estados unidos",
-    tasaMomentanea: "1000",
+    tasaMomentanea: "850.75",
     codigoIso:	"USD",
   },
   {
     moneda: "euro",
     simbolo: "€",
     ambito: "europa",
-    tasaMomentanea: "1100",
+    tasaMomentanea: "926.28",
     codigoIso:	"EUR",
   },
   {
     moneda: "real",
     simbolo: "R$",
     ambito: "brasil",
-    tasaMomentanea: "200",
+    tasaMomentanea: "170.32",
     codigoIso:"BRL",
   },
   
@@ -153,6 +205,4 @@ diferentesDivisas.sort(function(a, b) {
     return parseFloat(a.tasaMomentanea) - parseFloat(b.tasaMomentanea);
 });
 console.log("Divisas ordenadas por tasa de cambio:", diferentesDivisas);
-
-//cards
 
